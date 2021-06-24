c4cef43b206d1893179a3e98c14c2249be29ce74
guirehder <guilhermemrehder@gmail.com>
2021-06-24 09:22:21 -0300

    atualização dos testes de API com o método POST e GET

--- src/test/java/apiTests/testaPet.java
+++ src/test/java/apiTests/testaPet.java
@@ -12,6 +12,10 @@ import static org.hamcrest.Matchers.*;
 
 public class testaPet {
 
+    String uri = "https://petstore.swagger.io/v2/pet";
+    int petId = 1012;
+    String appjson = "application/json";
+
     // Padrão
     // Given = Dado
     // When = Quando
@@ -22,41 +26,43 @@ public class testaPet {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-        @Test
+        @Test (priority = 0)
     public void incluirPet() throws IOException { // Create - Post
 
             String jsonBody = lerJason("src/test/resources/data/pet.json");
 
             given()                                 // Dado
-                .contentType("application/json")    // Tipo de conteúdo
+                .contentType(appjson)    // Tipo de conteúdo
                     // "text/xml" para web services sincronos - ex: Correios
                     // "application/json" para web services assincronos - ex: iFood
                 .log().all()                        // Registrar tudo do envio
                 .body(jsonBody)
             .when()                                                 // Quando
-                .post("https://petstore.swagger.io/v2/pet")     // Comando + endpoint
+                .post(uri)     // Comando + endpoint
             .then()                                 // Então
                 .log().all()                        // Registrar tudo da volta
                 .statusCode(200)                    // Código do Estado
-                .body("id", is(1012))    // Valida o Id do animal
+                .body("id", is(petId))    // Valida o Id do animal
                 .body("name", is("Kiara"))    // Valida o Nome do animal
                 .body("category.name", is("dog"))    // Valida a categoria do animal
-                //.body("tags.name", hasItem("vermifugado"))    // Valida se contem a palavra chave
-                //.body("tags.name", contains("vacinas em dia"))    // Valida se contem a palavra chave
+                .body("tags.id[0]", is(0))
+                .body("tags.name[0]", stringContainsInOrder("primeira semana"))
+                .body("tags.id[1]", is(1))
+                .body("tags.name[1]", stringContainsInOrder("segunda semana"))
             ;
 
     }
 
-    @Test
+    @Test (priority = 1, dependsOnMethods = {"incluirPet"})
     public void consultarPet()  {
 
         String petId = "1012"; // Id do animal
 
         given()
-                .contentType("application/json")
+                .contentType(appjson)
                 .log().all()
                 .when()
-                .get("https://petstore.swagger.io/v2/pet/" + petId)   // Get == Consultar
+                .get(uri + "/" + petId)   // Get == Consultar
                 .then()
                 .log().all()
                 .statusCode(200)
@@ -64,4 +70,44 @@ public class testaPet {
                 .body("status", is("available"))
         ;
     }
+
+    @Test (priority = 2, dependsOnMethods = {"consultarPet"})
+    public void alterarPet() throws IOException {
+
+        String jsonBody = lerJason("src/test/resources/data/newpet.json");
+
+        given()
+            .contentType("")
+            .contentType(appjson)
+            .log().all()
+            .body(jsonBody)
+        .when()
+            .put(uri)
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("status", is("sold"))
+        ;
+    }
+
+    @Test (priority = 3, dependsOnMethods = {"incluirPet"})
+    public void excluirPet(){
+
+        given()
+                .contentType(appjson)
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is("1012"))
+        ;
+
+    }
+
+
+
 }
