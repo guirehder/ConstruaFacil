bca87145527b6040cf165c66e64960de9e135e6e
guirehder <guilhermemrehder@gmail.com>
2021-08-04 22:52:11 -0300

    Inclusao da home e comprar curso PO

---
+++ src/test/java/pages/Resultado.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Resultado {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    // O texto onde fica escrito Curso > "Nome do Curso"
+    @FindBy(css = "h3")
+    WebElement indicadorDeCurso;
+
+    @FindBy(css = "span.comprar")
+    WebElement btnMatriculese;
+
+    public Resultado(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public String lerIndicadorDeCurso() {
+        return indicadorDeCurso.getText();
+
+    }
+
+    public void clicarMatriculese() {
+        btnMatriculese.click();
+
+    }
+
+    public By verificarElemento() {
+        return By.cssSelector("h3");
+    }
+}
--- src/test/resources/features/comprarCursoCS.feature
+++ src/test/resources/features/comprarCursoCS.feature
@@ -7,3 +7,15 @@ Funcionalidade: : Comprar Curso CS
     Entao vejo a lista de resultados para o curso "Mantis"
     Quando clico no Matricule-se
     Entao confirmo o nome do curso como "Mantis" e o preco de "R$49,99"
+
+  Esquema do Cenario: Pesquisar Curso com Enter e Incluir no Carrinho EC
+    Dado que acesso o site da Iterasys
+    Quando pesquiso por <curso>
+    E pressiono Enter
+    Entao vejo a lista de resultados para o curso <curso>
+    Quando clico em Matricule-se
+    Entao confirmo o nome do curso como <curso> e o preco de <preco>
+    Exemplos:
+      | curso               |  preco      |
+      | "Mantis"            | "R$ 49,99"  |
+      | "Preparatório CTFL" | "R$ 169,00" |
